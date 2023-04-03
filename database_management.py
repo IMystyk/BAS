ac3cc5ca02dac8c064e668d04f9778dcef890ab9
Mystyk <mateuszr@zampub.rzeszow.pl>
2023-04-03 11:52:35 +0200

    Updated database tutorial file

--- database_management.py
+++ database_management.py
@@ -104,3 +104,54 @@ class Address(Base):
 
 
 Base.metadata.create_all(engine)
+#%%
+some_table = Table("some_table", metadata_obj, autoload_with=engine)
+#%%
+from sqlalchemy import insert
+stmt = insert(user_table).values(name="spongebob", fullname="Spongebob Squarepants")
+
+compiled = stmt.compile()
+#%%
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    conn.commit()
+#%%
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(user_table),
+        [
+            {"name": "sandy", "fullname": "Sandy Cheeks"},
+            {"name": "patric", "fullname": "Patrick Star"},
+        ],
+    )
+    conn.commit()
+#%%
+from sqlalchemy import select, bindparam
+
+scalar_subq = (
+    select(user_table.c.id).where(user_table.c.name == bindparam("username")).scalar_subquery()
+)
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(address_table).values(user_id=scalar_subq),
+        [
+            {
+                "username": "spongebob",
+                "email_address": "spongebob@sqlalchemy.org",
+            },
+            {"username": "sandy", "email_address": "sandy@sqlalchemy.org"},
+            {"username": "sandy", "email_address": "sandy@squirrelpower.org"},
+        ],
+    )
+    conn.commit()
+#%%
+insert_stmt = insert(address_table).returning(
+    address_table.c.id, address_table.c.email_address
+)
+print(insert_stmt)
+#%%
+select_stmt = select(user_table.c.id, user_table.c.name + "@aol.com")
+insert_stmt = insert(address_table).from_select(
+    ["user_id", "email_address"], select_stmt)
+print(insert_stmt.returning(address_table.c.id, address_table.c.email_address))
+#%%
